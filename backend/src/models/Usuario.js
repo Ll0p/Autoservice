e3f34c46a6915375386de36386c8d2540c8eee80
Tomas Llop <tomasignaciollop@gmail.com>
2026-06-03 14:10:44 -0300

    Mini ajuste 2

--- backend/src/models/Usuario.js
+++ backend/src/models/Usuario.js
@@ -4,9 +4,9 @@ import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
 
 const Usuario = sequelize.define("Usuario", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  correo: { type: DataTypes.STRING, allowNull: false, unique: true },
-  contrasenia: { type: DataTypes.STRING, allowNull: false },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    correo: { type: DataTypes.STRING, allowNull: false, unique: true },
+    contrasenia: { type: DataTypes.STRING, allowNull: false },
 }, { tableName: "usuarios" , timestamps: false });
 
 export default Usuario;
