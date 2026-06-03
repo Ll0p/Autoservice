ad3b7b44628e6c11bcdd7e7f495befb78aeb1444
Tomas Llop <tomasignaciollop@gmail.com>
2026-06-03 14:07:47 -0300

    Mini ajuste

--- backend/src/models/Usuario.js
+++ backend/src/models/Usuario.js
@@ -7,6 +7,6 @@ const Usuario = sequelize.define("Usuario", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   correo: { type: DataTypes.STRING, allowNull: false, unique: true },
   contrasenia: { type: DataTypes.STRING, allowNull: false },
-}, { tableName: "usuarios" , timestamps: true});
+}, { tableName: "usuarios" , timestamps: false });
 
 export default Usuario;
