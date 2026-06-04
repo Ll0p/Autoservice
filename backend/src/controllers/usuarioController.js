14467a86380c064189fbb3d1fee19f45c8b0a8d9
Tomas Llop <tomasignaciollop@gmail.com>
2026-06-03 21:08:54 -0300

    Segunda version controllers usuario

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -1,18 +1,8 @@
-import { Usuario } from "../models/index.js";
-import { UsuarioExistenteError, ServerError, ErrorInterno } from "../errors/ErrorApp.js"
-import { encriptarContrasenia } from "../services/usuarioServices.js";
+import { ServerError, ErrorInterno } from "../errors/ErrorApp.js"
+import { registrarNuevoUsuario } from "../services/usuarioServices.js";
 
 export function createUsuario(req, res) {
     const { correo, contrasenia } = req.body;
-
-    const verificarDisponibilidad = (usuarioExistente) => {
-        if (usuarioExistente) throw new UsuarioExistenteError();
-        return contrasenia;
-    };
-
-    const guardarUsuario = (contraseniaEncriptada) => {
-        return Usuario.create({ correo, contrasenia: contraseniaEncriptada });
-    };
 
     const responderExito = (nuevoUsuario) => {
         return res.status(201).json({id: nuevoUsuario.id, correo: nuevoUsuario.correo, mensaje: "Usuario administrador creado correctamente"});
@@ -24,11 +14,8 @@ export function createUsuario(req, res) {
         const errorInesperado = new ErrorInterno();
         return res.status(errorInesperado.statusCode).json(errorInesperado.aResponse());
     };
-
-    Usuario.findOne({ where: { correo } })
-        .then(verificarDisponibilidad)
-        .then(encriptarContrasenia) 
-        .then(guardarUsuario)       
+      
+    registrarNuevoUsuario(correo, contrasenia)
         .then(responderExito)       
         .catch(manejarErrores);     
 }
--- backend/src/services/usuarioServices.js
+++ backend/src/services/usuarioServices.js
@@ -1,5 +1,19 @@
 import bcrypt from "bcrypt";
+import { Usuario } from "../models/index.js";
+import { UsuarioExistenteError } from "../errors/ErrorApp.js";
 
-export async function encriptarContrasenia(contrasenia) {
-    return await bcrypt.hash(contrasenia, 10);
+export function registrarNuevoUsuario(correo, contrasenia) {
+    const verificarDisponibilidad = (usuarioExistente) => {
+        if (usuarioExistente) throw new UsuarioExistenteError();
+        return contrasenia;
+    };
+
+    const encriptarContrasenia = (contra) => bcrypt.hash(contra, 10);
+
+    const guardarUsuario = (contraseniaEncriptada) => Usuario.create({ correo, contrasenia: contraseniaEncriptada });
+
+    return Usuario.findOne({ where: { correo } })
+        .then(verificarDisponibilidad)
+        .then(encriptarContrasenia)
+        .then(guardarUsuario)
 }
