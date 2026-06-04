0a14032562edf71cb6bb2830be58aae56292cd2d
Tomas Llop <tomasignaciollop@gmail.com>
2026-06-04 12:53:55 -0300

    Posible reutilizacion de encriptarContraseña

--- backend/src/services/usuarioServices.js
+++ backend/src/services/usuarioServices.js
@@ -2,13 +2,15 @@ import bcrypt from "bcrypt";
 import { Usuario } from "../models/index.js";
 import { UsuarioExistenteError } from "../errors/ErrorApp.js";
 
+function encriptarContrasenia(contrasenia) {
+    return bcrypt.hash(contrasenia, 10);
+}
+
 export function registrarNuevoUsuario(correo, contrasenia) {
     const verificarDisponibilidad = (usuarioExistente) => {
         if (usuarioExistente) throw new UsuarioExistenteError();
         return contrasenia;
     };
-
-    const encriptarContrasenia = (contra) => bcrypt.hash(contra, 10);
 
     const guardarUsuario = (contraseniaEncriptada) => Usuario.create({ correo, contrasenia: contraseniaEncriptada });
 
